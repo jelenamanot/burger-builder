6c8d34d5d95b2f411a3eba1616e588f4996ea631
jelenanesic <jelenanesicm@gmail.com>
2017-12-27 10:50:54 +0100

    Orders fetching & styling

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients:</p>
-        <p>Price:<strong>5</strong></p>
-    </div>
-);
+import {formatPrice} from '../../assets/utils/helpers';
+
+const order = (props) => {
+    const ingredients = [];
+    for(let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+            }
+        );
+    }
+
+    const spanStyle = {
+        textTransform: 'capitalize',
+        display: 'inline-block',
+        margin: '0 8px',
+        border: '1px solid #ccc',
+        padding: '5px'
+    };
+
+    const ingredientOutput = ingredients.map(ig => <span key={ig.name} style={spanStyle}>{ig.name}</span>);
+
+    return(
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>{formatPrice(props.price)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
